8ae1f6d6f2f339bab709c59aeebe2522e94ac19c
Henri Kelder <henrikelder@gmail.com>
2018-01-31 18:11:02 +0200

    MuMu's first steps

---
+++ Assets/MuMu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuMu : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+        }
+    }
+}
